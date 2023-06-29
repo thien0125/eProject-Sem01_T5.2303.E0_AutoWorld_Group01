3b4ee594d6538f35abdf505e707e3624475bfa1c
janile0211 <137066060+janile0211@users.noreply.github.com>
2023-06-29 20:55:45 +0700

    myCtrl.js

--- myCtrl.js
+++ myCtrl.js
@@ -1,139 +1,123 @@
-//0. khai báo biến
-    var app = angular.module("myApp",["ngRoute"]);
+// 0. Khai báo biến
+var app = angular.module("myApp", ["ngRoute"]);
 
-//1. Dùng $routeProvider cấu hình route mở các single page khác
-    app.config(function($routeProvider)
-    {
-        $routeProvider
-        .when("/",{
-            templateUrl: "03_Home.html"
+// 1. Sử dụng $routeProvider để cấu hình route cho các single page khác nhau
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: "01_Home.html"
+        })
+        .when("/", {
+            templateUrl: "01_Home.html"
         })
         .when("/02_Itemlist", {
             templateUrl: "02_Itemlist.html",
             controller: "myCtrl"
         })
-    });
+        .when("/03_Comparison", {
+            templateUrl: "03_Comparison.html",
+            controller: "myCtrl"
+        })
+        .when("/05_Contact", {
+            templateUrl: "05_Contact.html",
+            controller: "myCtrl"
+        })
+        .when("/04_Payment", {
+            templateUrl: "04_Payment.html",
+            controller: "myCtrl"
+        })
+        ;
+        
+        
+});
 
-//2. phát triển controller
-    app.controller('myCtrl', function($scope, $http, $window, $location)
+// 2. Phát triển controller
+app.controller('myCtrl', function ($scope, $http, $window, $location) {
+    // 2.1 getData(): Lấy dữ liệu từ database.json
+    function getData() 
     {
-        //2.1 getData()     Lấy dữ liệu từ database.json
-            //tạo hàm
-                function getData()
-                    {
-                        $http.get("database.json")
-                        .then(function(x)
-                        {
-                            if (sessionStorage.getItem("n")==null)
-                            {
-                                //nếu null thì ghi giá trị vào
-                                    sessionStorage.setItem("n", JSON.stringify(x.data));
-                                //lấy data
-                                    $scope.database = JSON.parse(sessionStorage.getItem("n"));
-                            }
-                            else
-                            {
-                                //lấy data
-                                $scope.database = JSON.parse(sessionStorage.getItem("n"));
-                            }
-                        });
-                    }
-
-            //gọi hàm
-                getData();
-
-        //2.3 addData()     Thêm dữ liệu vào database.json
-            $scope.addData = function()
-            {
-                var add = 
-                {
-                    Stt: $scope.database.length,
-                    Brand: $scope.Brand,
-                    Model: $scope.Model,
-                    Image: $scope.Image,
-                    Price: $scope.Price
-                };
-
-                if (!$scope.Brand || !$scope.Model || !$scope.Price) 
-                {
-                    $window.alert("Can't leave any field blank");
+        $scope.selectedOption1 = 0;
+        $scope.selectedOption2 = 0;
+        $http.get("database.json")
+            .then(function (response) {
+                if (sessionStorage.getItem("n") == null) {
+                    // Nếu null thì ghi giá trị vào
+                    sessionStorage.setItem("n", JSON.stringify(response.data));
+                    // Lấy data
+                    $scope.database = JSON.parse(sessionStorage.getItem("n"));
+                } else {
+                    // Lấy data
+                    $scope.database = JSON.parse(sessionStorage.getItem("n"));
                 }
-                    else 
-                    {
-                        $scope.database.push(add);
-                        setStorage();
-                        var message = [];
-                        message.push("Data input successfull");
-                        alert(message.join());
-                        location = "index.html#!/02_Itemlist";
-                    };
+            });
+    }
 
-            }
-        //2.4 deleteData()  Xóa dữ liệu từ database.json
-        $scope.deleteData = function(add) 
-        {
-            var msg = "Are you sure to delete this record?";
-            if (confirm(msg)) {
-                var index = $scope.database.indexOf(add);
-                $scope.database.splice(index, 1);
-                setStorage();
-                $location.path("/02_Itemlist"); // Chuyển đến trang danh sách các mục sau khi xóa thành công
-            }
+    // Gọi hàm getData()
+    getData();
+
+    // 2.6 sortData()
+    $scope.sort = {};
+    $scope.sortData = function (cot) {
+        var sort = $scope.sort;
+        if (sort.cot === cot) {
+            // Đảo chiều sắp xếp sort.descending
+            sort.descending = !sort.descending;
+        } else {
+            sort.cot = cot;
+            // Khóa sort.descending = false
+            sort.descending = false;
         }
+    };
 
-        //2.5 hàm ghi vào session
-                function setStorage()
-                {
-                    var data = JSON.stringify ($scope.database);
-                    sessionStorage.setItem("n",data);
-                }
+   // Compare car
+   $scope.item1 = function(a) {
+    var select2 = $scope.selectedOption2;
+    if (a != select2) {
+        $scope.Image1 = $scope.database[a].Image;
+        $scope.brand1 = $scope.database[a].Brand;
+        $scope.Design1 = $scope.database[a].Design;
+        $scope.year1 = $scope.database[a].Year;
+        $scope.area1 = $scope.database[a].Area;
+        $scope.color1 = $scope.database[a].Color;
+        $scope.Seatingcapacity1 = $scope.database[a].Seatingcapacity;
+        $scope.price1 = $scope.database[a].Price;
+    } else 
+    {
+        $scope.selectedOption1 = 0;
+        $scope.Image1 = $scope.database[0].Image;
+        $scope.brand1 = $scope.database[0].Brand;
+        $scope.Design1 = $scope.database[0].Design;
+        $scope.year1 = $scope.database[0].Year;
+        $scope.area1 = $scope.database[0].Area;
+        $scope.color1 = $scope.database[0].Color;
+        $scope.Seatingcapacity1 = $scope.database[0].Seatingcapacity;
+        $scope.price1 = $scope.database[0].Price;
 
-        //2.6 sortData()
-            $scope.sort = {};
-            $scope.sortData = function(cot) 
-            {
-            var sort = $scope.sort;
-            if (sort.cot === cot) 
-            {
-                // Đảo chiều sắp xếp sort.descending
-                sort.descending = !sort.descending;
-            } 
-            else 
-            {
-                sort.cot = cot;
-                // Khóa sort.descending = false
-                sort.descending = false;
-            }
-            };
-        
-        //2.7 Fetch record lên bản input
-            $scope.fillInputs = function(data) 
-            {
-                $scope.selectedData = angular.copy(data); // Sao chép dữ liệu vào đối tượng selectedData
-                $scope.Brand = $scope.selectedData.Brand;
-                $scope.Model = $scope.selectedData.Model;
-                $scope.Price = $scope.selectedData.Price;
-            };
-        
-        //2.8 Update current record
-            $scope.updateData = function() 
-            {
-                var index = $scope.database.findIndex(function(item) 
-                {
-                    return item.Brand === $scope.Brand;
-                });
-            
-                if (index !== -1) 
-                {
-                    $scope.database[index].Brand = $scope.Brand;
-                    $scope.database[index].Model = $scope.Model;
-                    $scope.database[index].Price = $scope.Price;
-                    setStorage();
-                }
-            };
-        //2.9 Sort trong datalist
-            //chưa làm dc, nó chỉ sort cột đầu tiên trong table
-        
-        
-        
-    });
+    }
+    $scope.selectedCar1 = $scope.database[a];
+};
+
+$scope.item2 = function(a) {
+    var select1 = $scope.selectedOption1;
+    if (a != select1) {
+        $scope.Image2 = $scope.database[a].Image;
+        $scope.brand2 = $scope.database[a].Brand;
+        $scope.Design2 = $scope.database[a].Design;
+        $scope.year2 = $scope.database[a].Year;
+        $scope.area2 = $scope.database[a].Area;
+        $scope.color2 = $scope.database[a].Color;
+        $scope.price2 = $scope.database[a].Price;
+    } else {
+        $scope.selectedOption2 = 0;
+        $scope.Image2 = $scope.database[0].Image;
+        $scope.brand2 = $scope.database[0].Brand;
+        $scope.Design2 = $scope.database[0].Design;
+        $scope.year2 = $scope.database[0].Year;
+        $scope.area2 = $scope.database[0].Area;
+        $scope.color2 = $scope.database[0].Color;
+        $scope.Seatingcapacity2 = $scope.database[0].Seatingcapacity;
+        $scope.price2 = $scope.database[0].Price;
+    }
+    $scope.selectedCar2 = $scope.database[a];
+};
+});
